b1166ec3d1159cf4bdb4adbdb8339aba98152bfe
SSEO5 <wjdtjdud530@naver.com>
2024-05-14 02:19:31 +0900

    이미지 S3 업로드 기능

---
+++ src/main/java/com/clover/plogger/item/ItemController.java
@@ -0,0 +1,27 @@
+package com.clover.plogger.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final S3Service s3Service;
+
+    @GetMapping("/item")
+    String item() {
+        return "item.html";
+    }
+
+    @GetMapping("/presigned-url")
+    @ResponseBody
+    String getURL(@RequestParam String filename){
+        var result = s3Service.createPresignedUrl("test/" + filename);
+        return result;
+    }
+
+}
